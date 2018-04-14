c4ff1bede7274449c78a40fa7097ca68724e7cc8
kosmo24 <bl085@naver.com>
2018-04-14 11:12:10 +0900

    aaa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HomeComponent } from './home/home.component';
+import { JqueryComponent } from './jquery/jquery.component';
+import { AngularComponent } from './angular/angular.component';
+import {RouterModule, Routes} from '@angular/router';
 
+const routes: Routes = [
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HomeComponent,
+    JqueryComponent,
+    AngularComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(route),
   ],
   providers: [],
   bootstrap: [AppComponent]
