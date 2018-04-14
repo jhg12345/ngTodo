51608ff7511632835d76d0e74ea3b0e8fa3fd8b3
kosmo24 <bl085@naver.com>
2018-04-14 17:30:04 +0900

    다시 올리기

---
+++ src/app/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {TodoVO} from './domain/todo.vo';
+import {environment} from '../environments/environment';
+
+@Injectable()
+export class UserService {
+
+  private SERVER: string;
+  private headers: HttpHeaders;
+  constructor(private http: HttpClient) {
+    this.SERVER = `${environment.HOST}`;
+    this.headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+  }
+
+  getTodoList(): Observable<TodoVO[]> {
+    return this.http.get<TodoVO[]>(this.SERVER + '/api/todo');
+  }
+}
