8e634e312f06b5d905711c1a6526205b01727fbe
kosmo24 <bl085@naver.com>
2018-04-14 16:43:14 +0900

    aaa

--- src/app/angular/angular.component.ts
+++ src/app/angular/angular.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../user.service';
+import {TodoVO} from '../domain/todo.vo';
 
 @Component({
   selector: 'app-angular',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./angular.component.scss']
 })
 export class AngularComponent implements OnInit {
+  todoList: TodoVO[];
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.getTodoList()
+      .subscribe(body => {
+        this.todoList = body;
+        console.log(this.todoList);
+      });
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,19 @@ import { HomeComponent } from './home/home.component';
 import { JqueryComponent } from './jquery/jquery.component';
 import { AngularComponent } from './angular/angular.component';
 import {RouterModule, Routes} from '@angular/router';
+import {MatButtonModule, MatCardModule, MatIconModule, MatMenuModule, MatToolbarModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {HttpClientModule} from '@angular/common/http';
+import {UserService} from './user.service';
 
 const routes: Routes = [
-
+  {path: '', component: IndexComponent, children: [
+      {path: '', component: HomeComponent},
+      {path: 'jquery', component: JqueryComponent},
+      {path: 'angular', component: AngularComponent},
+    ]}, // 사용자 사이트
+//  {path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule'}, // 관리자 메뉴
 ];
 
 @NgModule({
@@ -23,9 +33,17 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(route),
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
