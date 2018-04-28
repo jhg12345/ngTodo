4b0a962e1aed2de5bda738e5cfd4613c308b88e1
kosmo24 <bl085@naver.com>
2018-04-28 11:26:57 +0900

    중간저장

--- src/app/angular/angular.component.ts
+++ src/app/angular/angular.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../user.service';
 import {TodoVO} from '../domain/todo.vo';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-angular',
   templateUrl: './angular.component.html',
-  styleUrls: ['./angular.component.scss']
+  styleUrls: ['./angular.component.scss'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({opacity: 1, transform: 'translate(0,0)'})),
+      transition('void => in', [
+        style({opacity: 0, transform: 'translate(-100%,0)'}),
+        animate(300)
+      ]),
+      transition('in => void', [
+        animate(300, style({opacity: 0, transform: 'translate(0,-100%)'}))
+      ])
+    ])
+  ]
 })
 export class AngularComponent implements OnInit {
   todoList: TodoVO[] = [];
