a0b9eac48ff6f57d021dd6300cc27eea65db2679
kosmo24 <bl085@naver.com>
2018-04-28 15:51:27 +0900

    custom directive 구현 모듈에 declaration 되어야 한다. ElementRef.nativeElement는 자바스크립트 노드를 리턴함. 외부로 값을 입력받는 방법은 @Input() 마우스 over, leave는 HostListener로 구현

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  @Input()
+  private highlightColor: string;
 
+  constructor(private el: ElementRef) {
+    // el.nativeElement는 node객체를 가지고 온다.
+    //el.nativeElement.style.backgroundColor = 'yellow';
+  }
+
+  @HostListener('mouseenter')
+  mouseEnter() {
+    this.el.nativeElement.style.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave')
+  mouseleave() {
+    this.el.nativeElement.style.backgroundColor = null;
+  }
 }
